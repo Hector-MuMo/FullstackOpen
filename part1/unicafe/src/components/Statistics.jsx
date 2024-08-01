9787c434838cea4b02da115dc0f930b3844efa76
Hector Muñoz M <hectormm@hobbio.co>
2024-08-01 10:15:45 -0600

    feat: create unicafe and add step 1

---
+++ part1/unicafe/src/components/Statistics.jsx
@@ -0,0 +1,13 @@
+
+const Statistics = ({ good, neutral, bad }) => {
+    return (
+        <div>
+            <h2>Statistics</h2>
+            <p>Good {good}</p>
+            <p>Neutral {neutral}</p>
+            <p>Bad {bad}</p>
+        </div>
+    )
+}
+
+export default Statistics

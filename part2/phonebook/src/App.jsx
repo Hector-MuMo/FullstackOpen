ab4d2364c0d2bfc251d7b025e1fad571119a7bd2
Hector Muñoz M <hectormm@hobbio.co>
2024-08-12 12:14:56 -0600

    feat: phonebook step 6 | fetch data from json server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import PersonsList from './components/PersonsList';
+import axios from 'axios';
 
 const Person = ({ info }) => {
     return (
@@ -12,12 +13,7 @@ const Person = ({ info }) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [filter, setFilter] = useState('');
@@ -57,6 +53,15 @@ const App = () => {
             alert('Please enter a name');
         }
     }
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons').
+            then(response => {
+                setPersons(response.data);
+            }
+            )
+    }, [])
+
 
     const personsList = filter === ''
         ? persons.map((item, index) => <Person key={index + 'a'} info={item} />)

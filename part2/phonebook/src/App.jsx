a2f0856a61c35cfd05abb28ed804041b8d396c20
Hector Muñoz M <hectormm@hobbio.co>
2024-08-12 10:34:15 -0600

    feat: phonebook step 4 | create filter

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,33 +10,68 @@ const Person = ({ info }) => {
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
-    ])
-    const [newName, setNewName] = useState('')
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ]);
+    const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [filter, setFilter] = useState('');
+    const [personsFiltered, setPersonsFiltered] = useState([]);
 
-    const handleChange = (e) => {
+    const handleChangeName = (e) => {
         setNewName(e.target.value);
     }
 
-    const handleAddPerson = (e) => {
-        e.preventDefault()
+    const handleChangeNumber = (e) => {
+        setNewNumber(e.target.value);
+    };
+
+    const handleFilterList = (e) => {
+        const regEx = new RegExp(e.target.value, 'i');
+
+        const filteredList = persons.filter(person => regEx.test(person.name));
+
+        setPersonsFiltered(filteredList);
+        setFilter(e.target.value);
+
+    }
+
+    const handleSubmitPerson = (e) => {
+        e.preventDefault();
 
         if (newName.length !== 0) {
-            setPersons([...persons, { name: newName }])
-            setNewName('')
+            const isInclude = persons.find(item => item.name === newName);
+            if (isInclude) {
+                alert(`${newName} is already added to phonebook`);
+            } else {
+                setPersons([...persons, { name: newName, number: newNumber }]);
+                setNewName('');
+                setNewNumber('');
+            }
         } else {
-            alert('Please enter a name')
+            alert('Please enter a name');
         }
     }
 
-    const personsList = persons.map((item, index) => <Person key={index + 'a'} info={item} />)
+    const personsList = filter === ''
+        ? persons.map((item, index) => <Person key={index + 'a'} info={item} />)
+        : personsFiltered.map((item, index) => <Person key={index + 'a'} info={item} />)
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form onSubmit={handleAddPerson}>
+            <div>
+                Filter shown with <input type="text" value={filter} onChange={e => handleFilterList(e)} />
+            </div>
+            <h2>Add new</h2>
+            <form onSubmit={handleSubmitPerson}>
                 <div>
-                    name: <input value={newName} onChange={(e) => handleChange(e)} />
+                    name: <input value={newName} onChange={(e) => handleChangeName(e)} />
+                </div>
+                <div>
+                    number: <input value={newNumber} onChange={handleChangeNumber} />
                 </div>
                 <div>
                     <button type="submit">add</button>
